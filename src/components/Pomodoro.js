778d29e61644a5564db185921aea1dae818c90f9
Arsh <arshdeepgary@gmail.com>
2020-03-31 21:22:44 -0700

    trying to figure out things

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 let log = console.log;
 let timer;
+
 export default function Pomodoro() {
     const[pomoTime, setPomoTime] = useState(25);
 
@@ -17,11 +18,17 @@ export default function Pomodoro() {
         clearInterval(timer);
     }
 
+    let handleReset = () => {
+        setPomoTime(25);
+        clearInterval(timer);
+    }
+
     return(
         <div>
             {pomoTime}
             <button onClick={handleStart}>Start</button>
             <button onClick={handlePause}>Pause</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 }
